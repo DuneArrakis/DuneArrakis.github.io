c92cbcce646461b8f4f7e43167b7e624a211af9b
DuneArrakis <iris_zztao@163.com>
2025-03-14 14:33:34 +0800

    鑷姩鎻愪氦

--- open bash.bat
+++ open bash.bat
@@ -1,1 +1,12 @@
-start D:\Git\git-bash.exe --cd=E:\github\DuneArrakis.github.io && git add . && git commit -a -m "添加内容" && git push
+@echo off
+rem 设置 Git Bash 的安装路径和项目所在目录
+set "GIT_BASH_PATH=D:\Git\git-bash.exe"
+set "PROJECT_PATH=E:\github\DuneArrakis.github.io"
+
+rem 切换到项目目录
+cd /d %PROJECT_PATH%
+
+rem 按顺序执行命令：git add .; git commit -a -m "自动提交"; git push
+%GIT_BASH_PATH% -c "git add .; git commit -a -m \"自动提交\"; git push"
+
+pause
