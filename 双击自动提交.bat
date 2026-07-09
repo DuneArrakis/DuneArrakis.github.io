97705343e8151f6e0a8e6ba3f752679d1e201096
DuneArrakis <Iris_zztao@163.com>
2026-07-09 16:46:47 +0800

    Update auto commit script

--- 双击自动提交.bat
+++ 双击自动提交.bat
@@ -1,12 +1,15 @@
 @echo off
+chcp 65001 >nul
 
-chcp 65001
-
-set "GIT_BASH_PATH=D:\Git\git-bash.exe"
+set "GIT_BASH_PATH=D:\Git\bin\bash.exe"
 set "PROJECT_PATH=E:\github\DuneArrakis.github.io"
 
-cd /d %PROJECT_PATH%
+cd /d "%PROJECT_PATH%" || (
+  echo Cannot enter project folder: "%PROJECT_PATH%"
+  pause
+  exit /b 1
+)
 
-%GIT_BASH_PATH% -c "git add .; git commit -a -m \"自动提交\"; git push"
+"%GIT_BASH_PATH%" -lc "cd /e/github/DuneArrakis.github.io && git add -A && if git diff --cached --quiet; then echo No changes to commit.; else git commit -m 'auto commit' && git push; fi"
 
 pause
